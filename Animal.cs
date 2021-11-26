75e44fc5fb56e00fc52da6a11737967bf19066ff
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-11-26 21:01:23 +0100

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uses_OF_Lists
+{
+    public class Animal
+    {
+        public string name;
+        public int age;
+        
+        public Animal(string Name , int Age)
+        {
+            this.name = Name;
+            this.age = Age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Uses_OF_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> Animales = new List<Animal>();
+            Animales.Add(new Animal("Panda", 25));
+            Animales.Add(new Animal("Monkey", 10));
+            Animales.Add(new Animal("Dankey", 30));
+
+            foreach (var animal in Animales)
+            {
+                Console.WriteLine($"Name: {animal.name} & Age: {animal.age}");
+            }
+        }
+    }
+}
